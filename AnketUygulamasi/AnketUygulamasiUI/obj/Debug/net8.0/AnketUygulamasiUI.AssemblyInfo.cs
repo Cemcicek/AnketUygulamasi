029a7e3cf010e36a832ceeeeba5b52161a7856dc
Cem Çiçek <57458396+Cemcicek@users.noreply.github.com>
2024-07-27 13:59:41 +0300

    project

--- AnketUygulamasi/AnketUygulamasiUI/obj/Debug/net8.0/AnketUygulamasiUI.AssemblyInfo.cs
+++ AnketUygulamasi/AnketUygulamasiUI/obj/Debug/net8.0/AnketUygulamasiUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnketUygulamasiUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20f3d5980f830598f2f05735cf783ecd98e982d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+068dcfcedf37ad8d597ea6583c9c045f499832ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnketUygulamasiUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnketUygulamasiUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
